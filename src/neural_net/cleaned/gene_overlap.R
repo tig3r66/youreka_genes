6e64e12f59bb2be4db9758344fd606504839684e
Eddie Guo <eguo1@ualberta.ca>
2021-11-15 15:33:14 -0700

    redownloaded from icloud

--- src/neural_net/cleaned/gene_overlap.R
+++ src/neural_net/cleaned/gene_overlap.R
@@ -1,4 +1,4 @@
-library(dplyr)
+library(tidyverse)
 
 blist <- read_csv("boruta.csv", col_names=F)
 reflist <- read_csv("cancer_gene_census.csv", col_names = F)
--- src/neural_net/cleaned/gene_overlap.py
+++ src/neural_net/cleaned/gene_overlap.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-blist = pd.read_csv("boruta.csv", names=['blist'])
-ref_list = pd.read_csv("cancer_gene_census.csv", names=['ref_list'])
+blist = pd.read_csv("boruta.csv", names=['genes'])
+ref_list = pd.read_csv("cancer_gene_census.csv", names=['genes'])
 
-blist = set(blist['blist'])
-ref_list = set(ref_list['ref_list'])
+blist = set(blist['genes'])
+ref_list = set(ref_list['genes'])
 
-print(blist.intersection(ref_list))
+print(*blist.intersection(ref_list), sep='\n')
 print(len(blist.intersection(ref_list)))
+
+# for idx, item in enumerate(ref_list):
+# 	if item in full_list['Gene Symbol']:
+# 		print(full_list['Gene Symbol'][idx])
